8b756883126a05b7a47f610dd11b906c6f429f23
Matthias <leuffen@continue.de>
2015-11-18 20:15:56 +0100

    Angular Bridge controller umbau

---
+++ src/server/v10/js/ANGULAR/README.md
@@ -0,0 +1,50 @@
+# Using DOTJS Angular Bridge
+
+## Activating the bridge
+
+In your template just add
+
+```
+
+<body>
+..
+    <script remote="remote">
+        DOT.include("dot://ANGULAR/ANGULAR.js");
+        DOT.ANGULAR.printAngularInit();
+    </script>
+</body>
+
+```
+
+This will generate a brower-side stub. Please make sure to manually require the 
+vendor Angular javascript code.
+
+## Using the Bridge
+
+You can access the observed object by using and modifying `$$observed` which should be
+the last parameter of your Action.
+
+```
+<script remote>
+
+    /* This is the SERVER - Side */
+    CTRL.prototype.someAction = function (param1, param2, $$observed) {
+        $$observed.someText = "You said: " + $$observed.textBox;
+    }
+</script>
+
+<script language="JavaScript">
+    /* Browser-Side */
+    angular.module("SiteApp", ["$dotremote"]).controller("TestCtrl", function ($dotremote) {
+
+        var remote = $dotremote.register(this);
+        
+        this.someAction = function () {
+            remote.someAction("wurst");
+        };
+    });
+
+</script>
+```
+
+You'll immediatly benefit from Client<>Server Code-Completion in your ide.
--- src/server/v10/js/ANGULAR/browser/class/CTRLProxy.js
+++ src/server/v10/js/ANGULAR/browser/class/CTRLProxy.js
@@ -10,6 +10,34 @@
         "use strict";
         var self = this;
 
+        this.className = "CTRLProxy";
+
+        var _evaluateReturnOptions = function (returnOptions, lastCallPromise) {
+            $log.debug("[DOT][ANGULAR][CTRLProxy] Evaluating returnOptions: ", returnOptions);
+            if (typeof returnOptions.event === "object") {
+                var event = returnOptions.event;
+                if (returnOptions.preventDefault === true) {
+                    $log.debug("[DOT][ANGULAR][CTRLProxy] preventDefault() (Browser)");
+                    event.preventDefault();
+                }
+                if (returnOptions.stopPropagation === true) {
+                    $log.debug("[DOT][ANGULAR][CTRLProxy] Stopping event propagation (Browser)");
+                    event.stopPropagation();
+                }
+
+            }
+
+            if (returnOptions.return === "promise")
+                return lastCallPromise;
+
+            if (typeof returnOptions.return === "function")
+                return returnOptions.return();
+
+            return returnOptions.return;
+        };
+
+
+
         // Register all Actions to this object
         // On EcmaScript 6 we'll change this to use Proxy() class
         dotRemote.__call("getActions", {}, config)
@@ -22,13 +50,42 @@
 
 
                     self[curActionName] = function () {
+                        $log.debug("[DOTJS][ANGULAR][CTRLProxy] Action: '" + curActionName + "' called with:", arguments);
+
                         var params = [ curActionName ];
+                        var returnOptions = {
+                            return: "promise",
+                            stopPropagation: false,
+                            preventDefault: false,
+                            event: null
+                        };
                         for (var i2=0; i2<arguments.length; i2++) {
+                            if (typeof arguments[i2] === "object") {
+                                if (i2 > 0) {
+                                    $log.warn("[DOTJS][ANGULAR][CTRLProxy] Action: '" + curActionName + "': Only first argument is allowed to be other than string (Browser script).");
+                                    throw new Error("Action: '" + curActionName + "': Only first argument is allowed to be other than string (Browser script).");
+                                }
+                                var returnOptions = arguments[i2];
+                                params.push("$$");
+                                continue;
+                            }
                             params.push(arguments[i2]);
                         }
 
+                        // Collect data to transport as scope to client.
+                        var objectData = {};
+                        for (var name in observedObject) {
+                            if (typeof observedObject[name] === "function")
+                                continue;
+                            if (typeof observedObject[name] === "object") {
+                                if (observedObject[name].className === "CTRLProxy")
+                                    continue;
+                            }
+                            objectData[name] = observedObject[name];
+                        }
 
-                        return dotRemote.__call(params, observedObject, config)
+
+                        var promise = dotRemote.__call(params, objectData, config)
                             .then(function (response) {
                                 if (typeof response.data !== "object") {
                                     $log.warn("[DOTJS][ANGULAR][CTRLProxy] Got non json formated result: '" + response.data);
@@ -49,6 +106,7 @@
                                 return response.data.returned;
                             });
 
+                        return _evaluateReturnOptions(returnOptions, promise);
                     }
                 }
                 $log.debug("[DOTJS][ANGULAR][CTRLProxy] Available remote actions:", actionArr);
